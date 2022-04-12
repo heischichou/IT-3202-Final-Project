553b17b7506a6dd197f2a411efc8d2c66e6593d1
teddzyb <teddybartletg88@gmail.com>
2022-04-12 17:07:17 +0800

    ESLint fixes

--- src/components/UnitTest.vue
+++ src/components/UnitTest.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="message">{{ message }}</div>
-    <input v-model="username">
+    <input v-model="username" />
     <div v-if="error" class="error">{{ error }}</div>
   </div>
 </template>
@@ -11,16 +11,18 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "UnitTest",
-  data () {
+  data() {
     return {
-      message: 'Unit Test with Jest',
-      username: ''
-    }
+      message: "Unit Test with Jest",
+      username: "",
+    };
   },
   computed: {
     error(): string {
-      return this.username.trim().length < 7 ? 'Please enter a longer username' : '';
-    }
-  }
+      return this.username.trim().length < 7
+        ? "Please enter a longer username"
+        : "";
+    },
+  },
 });
 </script>
