87a0800d1ee25fe8bcb1ab3de4dcbfdf2ceff982
heischichou <jangarot@gmail.com>
2022-04-12 14:45:19 +0800

    Added Jest unit testing

---
+++ src/components/UnitTest.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="message">{{ message }}</div>
+    <input v-model="username">
+    <div v-if="error" class="error">{{ error }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UnitTest",
+  data () {
+    return {
+      message: 'Unit Test with Jest',
+      username: ''
+    }
+  },
+  computed: {
+    error(): string {
+      return this.username.trim().length < 7 ? 'Please enter a longer username' : '';
+    }
+  }
+});
+</script>
