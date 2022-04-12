7fd71a0c64bd4b68e27feead5034c4388729f581
teddzyb <teddybartletg88@gmail.com>
2022-04-12 23:54:35 +0800

    Added sample integration tests

--- tests/e2e/specs/sample-integration-test.js
+++ tests/e2e/specs/sample-integration-test.js
@@ -5,4 +5,12 @@ describe("Tests HelloWorld page", () => {
   it("should check plugin links", function () {
     cy.get("[data-cy=plugins]").children().should("have.length.greaterThan", 0);
   });
+  it("should check essential links", function () {
+    cy.get("[data-cy=essential]")
+      .children()
+      .should("have.length.greaterThan", 0);
+  });
+  it("should check ecosystem links", function () {
+    cy.get("[data-cy=eco]").children().should("have.length.greaterThan", 0);
+  });
 });
