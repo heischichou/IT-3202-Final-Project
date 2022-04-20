6b869fc781d4d4f45e1404a6b54779b101e6706a
teddzyb <teddybartletg88@gmail.com>
2022-04-20 19:29:52 +0800

    Added custom scss variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/styles/app.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
