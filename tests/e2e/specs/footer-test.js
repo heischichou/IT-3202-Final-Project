bc009d26b633afae83ad681772bee3269bcd3b22
Nina Therese <ninathereseybas@gmail.com>
2022-04-26 17:18:59 +0800

    [dev-52] added test for footer

---
+++ tests/e2e/specs/footer-test.js
@@ -0,0 +1,12 @@
+describe("Footer Content", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8081/");
+  });
+  it("should check if footer exists", function () {
+    cy.get("footer").should("be.visible");
+  });
+  it("should check text content within footer", function () {
+    cy.get("footer").contains("h6", "Designed in Figma. With Love");
+    cy.get("footer").contains("h6", "Ednifran . 2022");
+  });
+});
