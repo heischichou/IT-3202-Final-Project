85f3bec8dc6d6288b4c4319ff8c8e7648944e07e
Francine Menchavez <francia.menchavez054@gmail.com>
2022-04-28 18:16:18 +0800

    [dev-45] Added My Projects Page header

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="row fw-bold m-0 justify-content-end mt-4">
-    <div class="col-md-9 bg-dark">
-      <div class="container text-end bg-dark text-primary w-75 m-5">
-        <h1 class="mt-5 m-0" style="font-weight: 700; font-size: 100px">My</h1>
-        <h1 class="my-5 m-0" style="font-weight: 700; font-size: 100px">
-          Projects
-        </h1>
+  <div class="container-fluid">
+    <div class="row fw-bold mt-4">
+      <div class="container d-flex flex-row-reverse p-0">
+        <div class="col-md-9 bg-dark">
+          <div class="container text-end bg-dark text-primary w-75 p-0">
+            <h1 class="mt-5" style="font-weight: 700; font-size: 100px">My</h1>
+            <h1 class="my-5" style="font-weight: 700; font-size: 100px">
+              Projects
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   </div>
