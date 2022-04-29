023bc253b38605e2c29ab2777b1d1d385eae4c90
Nina Therese <ninathereseybas@gmail.com>
2022-04-30 00:12:25 +0800

    [dev-45] Added Tag component for Project Component

---
+++ src/components/ProjectTag.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn btn-sm p-tag bg-light">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "ProjectTag",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.p-tag {
+  position: static;
+  border-radius: 16px;
+}
+</style>
