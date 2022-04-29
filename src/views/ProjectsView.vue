b704643adac70ee016c7ecffb42de3a5398530d4
Francine Menchavez <francia.menchavez054@gmail.com>
2022-04-29 14:06:44 +0800

    [dev-45] Fixed My Projects header

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="container-fluid">
-    <div class="row fw-bold mt-4">
-      <div class="container d-flex flex-row-reverse p-0">
-        <div class="col-md-9 bg-dark">
-          <div class="container text-end bg-dark text-primary w-75 p-0">
-            <h1 class="mt-5" style="font-weight: 700; font-size: 100px">My</h1>
-            <h1 class="my-5" style="font-weight: 700; font-size: 100px">
-              Projects
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div
+    class="container-lg bg-dark me-0 mt-3 rounded-start"
+    style="height: 150px; margin-bottom: -100px"
+  ></div>
+  <div class="container text-end bg-transparent">
+    <h1 class="text-primary font-weight-bolder" style="font-size: 4.5rem">
+      My<br />Projects
+    </h1>
   </div>
+  <div
+    class="container-lg bg-dark me-0 mb-3 rounded-start"
+    style="height: 150px; margin-top: -100px"
+  ></div>
 </template>
 
 <script>
