9ac3247146301854a4fd2c2ba674605132c4bd6f
Edwin Bartlett <teddybartletg88@gmail.com>
2022-05-17 16:24:14 +0800

    [dev-46] Add header

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="container">
-    <h2>Skills View</h2>
+  <!-- Header -->
+  <div class="container-fluid d-flex flex-row p-0">
+    <div class="col-md-3"></div>
+    <div
+      class="col bg-dark rounded-1 me-0"
+      id="main"
+      style="height: 400px; margin-top: 80px; margin-bottom: -410px"
+    ></div>
   </div>
+  <div class="container bg-transparent mb-5">
+    <div class="row align-items-end bg-transparent mb-4">
+      <div class="col-md-auto container-fluid bg-transparent">
+        <div class="row bg-transparent mt-n5">
+          <h1 class="text-primary text-center text-md-start fw-bolder fs-xxl">
+            My <br />
+            Technical <br />
+            Skill Set
+          </h1>
+        </div>
+      </div>
+      <div
+        class="col-md container-fluid bg-transparent ms-md-6 mt-3 mt-md-auto"
+      >
+        <div class="row bg-transparent my-2">
+          <p class="text-center text-md-start">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div
+          class="col-auto text-center text-md-start bg-transparent mt-4 mt-md-auto"
+        >
+          <button class="btn btn-secondary px-4 py-3">CONTACT ME</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- This is just to add space between the footer and content; delete later. -->
+  <div class="my-5">&nbsp;</div>
 </template>
 
 <script>
@@ -10,4 +46,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/*div {*/
+/*  border: 1px solid black;*/
+/*}*/
+</style>
