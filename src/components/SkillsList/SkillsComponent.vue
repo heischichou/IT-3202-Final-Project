7577b74f104c7c933fc39b36670602cbc29c126b
Edwin Bartlett <teddybartletg88@gmail.com>
2022-05-21 11:34:07 +0800

    [dev-90] Optimize for mobile

--- src/components/SkillsList/SkillsComponent.vue
+++ src/components/SkillsList/SkillsComponent.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="mt-5 pt-5 container">
-    <h1 class="text-secondary fw-bold text-center mb-5">
+  <div class="mt-md-6 pt-md-5 container">
+    <h1 class="text-secondary fw-bold text-center mb-4 mb-md-6">
       {{ item.skill_name }}
     </h1>
     <div
-      class="d-flex justify-content-center p-0"
-      :class="index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'"
+      class="d-flex flex-column justify-content-center p-0"
+      :class="index % 2 === 0 ? 'flex-md-row' : 'flex-md-row-reverse'"
     >
       <div
-        class="d-flex flex-row align-items-center"
-        :class="index % 2 === 0 ? 'col-6' : 'col-auto'"
+        class="d-flex flex-row align-items-center justify-content-center justify-content-md-start"
+        :class="index % 2 === 0 ? 'col-auto col-md-5' : 'col-auto'"
       >
         <img
           class="rounded-2"
-          style="height: 360px"
+          style="height: 420px"
           src="../../../src/assets/logo.png"
           alt="Skill Image"
         />
       </div>
       <div
-        class="d-flex flex-row align-items-center"
-        :class="index % 2 === 0 ? 'col-auto' : 'col-6'"
+        class="d-flex flex-row align-items-center justify-content-center justify-content-md-start mt-3 mt-md-0"
+        :class="index % 2 === 0 ? 'col-auto' : 'col-auto col-md-5'"
       >
         <ul class="m-0">
           <li
-            class="py-1"
+            class="py-2"
             v-for="(skill, index) in item.skill_list"
             :key="index"
           >
