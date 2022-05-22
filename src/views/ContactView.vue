f3289aa446bb3a1b53629192a9985734a53c7c56
Francine Menchavez <francia.menchavez054@gmail.com>
2022-05-22 15:48:29 +0800

    [dev-47] Create header for Contact Me Page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="container">
-    <h2>Contact View</h2>
+  <div
+    class="container-fluid d-flex flex-row bg-dark p-0"
+    style="height: 500px; margin-top: -100px"
+  >
+    <div class="container d-flex bg-dark justify-content-center">
+      <div class="row bg-transparent">
+        <div class="row bg-transparent align-items-end">
+          <div class="bg-transparent text-center" style="margin-top: 200px">
+            <h1
+              class="font-weight-bolder text-primary fw-bold"
+              style="font-size: 4.5rem"
+            >
+              Get in touch
+            </h1>
+          </div>
+        </div>
+        <div class="row bg-transparent align-items-start">
+          <div class="col bg-transparent p-0 text-center">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempory
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +34,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/*div {*/
+/*  border: 1px;*/
+/*  outline: solid black;*/
+/*}*/
+</style>
