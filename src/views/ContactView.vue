0d2845307b582c0e0822c5969fc8ae52f9edf947
Francine Menchavez <francia.menchavez054@gmail.com>
2022-05-23 17:08:27 +0800

    [dev-47] Add Frequently Asked Q&A

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -26,9 +26,9 @@
       </div>
     </div>
   </div>
-  <div class="container mt-5 pt-5">
+  <div class="container mt-5">
     <div class="row justify-content-center">
-      <div class="col-9 col-md-3 my-3">
+      <div class="col-9 col-md-4 my-3">
         <div class="card bg-dark rounded-3">
           <div class="card-header rounded-3 bg-lightblue-1">
             <div class="container d-flex justify-content-center bg-transparent">
@@ -45,7 +45,7 @@
           </div>
         </div>
       </div>
-      <div class="col-9 col-md-3 my-3">
+      <div class="col-9 col-md-4 my-3">
         <div class="card bg-dark rounded-3">
           <div class="card-header rounded-3 bg-lightblue-1">
             <div class="container d-flex justify-content-center bg-transparent">
@@ -64,11 +64,70 @@
       </div>
     </div>
   </div>
+  <div class="container d-flex justify-content-center mt-5 pt-5">
+    <h3 class="text-secondary fw-bold mb-2">Send A Message</h3>
+    <div class="card bg-lightblue-1 opacity-75 rounded-2">EMAIL HERE</div>
+  </div>
+  <div class="container text-center mt-5 pt-5">
+    <h2 class="text-secondary fw-bold">Frequently Asked Questions</h2>
+  </div>
+  <div class="container mb-5 p-5">
+    <div
+      class="row g-2 mt-4 ms-4 align-items-center"
+      v-for="QnA in QnAData"
+      :key="QnA.QnA_id"
+    >
+      <div class="col-2 col-md-4">
+        <div class="text-end text-darkblue fs-4">Q</div>
+      </div>
+      <div class="col-9 col-md-6">
+        <div class="ps-5 text-darkblue">{{ QnA.Question }}</div>
+      </div>
+      <div class="col-2 col-md-4">
+        <div class="text-end text-secondary fs-4 mt-0">A</div>
+      </div>
+      <div class="col-9 col-md-6 mt-0">
+        <div class="ps-5 text-secondary">{{ QnA.Answer }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ContactView",
+  data() {
+    return {
+      QnAData: [
+        {
+          QnA_id: 1,
+          Question: "What are you best at?",
+          Answer: "Java",
+        },
+        {
+          QnA_id: 2,
+          Question: "Are you for hire?",
+          Answer: "Not at the moment",
+        },
+        {
+          QnA_id: 3,
+          Question: "Are you available for consultations?",
+          Answer: "Yes",
+        },
+        {
+          QnA_id: 4,
+          Question:
+            "What are the bleeding edge technologies you are familiar on? ",
+          Answer: "Federation GraphQL, Kotlin, AWS CDK",
+        },
+        {
+          QnA_id: 5,
+          Question: "What do you do on your free time?",
+          Answer: "Sort",
+        },
+      ],
+    };
+  },
 };
 </script>
 
