ed78c2ab29716bd7d30c782d540285b2567674fe
Edwin Bartlett <teddybartletg88@gmail.com>
2022-05-28 23:56:36 +0800

    [dev-97] Test Contact Details component

--- src/components/Contact/ContactDetails.vue
+++ src/components/Contact/ContactDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-12 col-md-5 col-lg-4 my-3 mx-4">
+  <div class="contact-info col-12 col-md-5 col-lg-4 my-3 mx-4">
     <div class="card bg-lightblue-1 bg-opacity-50 rounded-3 border-0">
       <div class="card-header bg-lightblue-1 bg-opacity-75 rounded-3 py-4">
         <div class="container d-flex justify-content-center bg-transparent">
