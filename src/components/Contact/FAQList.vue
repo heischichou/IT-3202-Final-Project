68b7e9b6ec472287fb1a41086388cca2d76e5a92
Edwin Bartlett <teddybartletg88@gmail.com>
2022-05-29 00:09:28 +0800

    [dev-103] Test FAQ component

--- src/components/Contact/FAQList.vue
+++ src/components/Contact/FAQList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container-fluid mb-4">
+  <div class="qna container-fluid mb-4">
     <div class="row align-items-center justify-content-center">
       <div class="col-3 col-md-2 col-xl-1">
         <p class="text-darkblue fs-4 fw-bold text-center">Q.</p>
       </div>
       <div class="col col-md-6 col-xl-4">
-        <p class="text-darkblue fs-5">{{ faq.question }}</p>
+        <p class="question text-darkblue fs-5">{{ faq.question }}</p>
       </div>
     </div>
     <div class="row align-items-center justify-content-center">
@@ -13,7 +13,7 @@
         <p class="text-secondary fs-4 fw-bold text-center">A.</p>
       </div>
       <div class="col col-md-6 col-xl-4">
-        <p class="text-secondary fs-5">{{ faq.answer }}</p>
+        <p class="answer text-secondary fs-5">{{ faq.answer }}</p>
       </div>
     </div>
   </div>
