aa975ce810804a765a8c63e067f162cf774adc0c
Edwin Bartlett <teddybartletg88@gmail.com>
2022-05-28 16:37:53 +0800

    [dev-87] Test Technology Stack component

--- src/components/SkillsList/TechnologyList.vue
+++ src/components/SkillsList/TechnologyList.vue
@@ -3,13 +3,13 @@
     class="technology row justify-content-between align-items-center my-3 my-md-4 my-lg-3 mx-3 mx-md-5"
   >
     <div class="col-6 col-md-12 col-lg order-first">
-      <h4>{{ item.name }}</h4>
+      <h4 class="tech-title">{{ item.name }}</h4>
     </div>
     <div
-      class="col col-md-12 col-lg order-last d-flex justify-content-between align-items-center"
+      class="tech-scale col col-md-12 col-lg order-last d-flex justify-content-between align-items-center"
     >
       <div
-        class="bg-darkblue rounded-pill"
+        class="tech-scales bg-darkblue rounded-pill"
         style="width: 25px; height: 25px"
         v-for="i in item.scale"
         :key="i"
