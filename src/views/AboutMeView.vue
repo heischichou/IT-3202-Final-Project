bc99d87d06c2157fea46a369034b4606ae351457
Edwin Bartlett <teddybartletg88@gmail.com>
2022-06-21 14:12:05 +0800

    [dev-33] Edit text

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -21,7 +21,7 @@
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequa.
+            aliquip ex ea commodo consequa!
           </p>
         </div>
         <div class="row gap-4 fw-bold bg-transparent mt-2 my-lg-4">
