538adfe1d5fa8bad9a6723ed1a97e1c6d5163019
Heischichou <jangarot@gmail.com>
2023-05-21 10:02:01 +0800

    Update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript",
+  bail: true,
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
+  moduleNameMapper: {
+    "^vue$": "vue/dist/vue.common.js"
+  },
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "\\.[jt]sx?$": 'babel-jest',
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: [
+    "js",
+    "vue"
+  ],
 };
