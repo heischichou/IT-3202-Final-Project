42ac12551eb8fe36d7f703ef61924be5eb9d22f5
Caerfyre <ninathereseybas@gmail.com>
2023-05-21 20:55:03 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript",
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   bail: true,
   verbose: true,
   transform: {
@@ -9,7 +9,21 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "js", "vue"],
   testEnvironment: "jsdom",
+  clearMocks: true,
+  coverageProvider: "v8",
   collectCoverage: true,
-  // collectCoverageFrom: [ 'src/components/**/*.{ts,vue}' ],
+  // collectCoverageFrom: [ 
+  //   'src/components/**/*.{ts,vue}',
+  //   "./tests/unit/**/*.{js,jsx,ts,tsx,vue}",
+  //   "!./tests/unit/**/_*.{js,jsx,ts,tsx,vue}",
+  // ],  
   coverageReporters: ["text"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+  testMatch: [
+    "<rootDir>/tests/unit/**/*.spec.ts",
+  ],
 };
